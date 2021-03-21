132ea6e9a9c732f568d7ddd9b24b65d9a7b794ac
itsrizals <44670534+itsrizals@users.noreply.github.com>
2021-03-21 15:14:19 +0700

    Test express method & change package.json information

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,28 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
+import { listings } from './listings'
+
 const app = express()
 const port = 9000
 
-app.get('/', (_req, res) => res.send('Hello, World!'))
+app.use(bodyParser.json())
+
+app.get('/', (_req, res) => res.send(`Hello World!`))
+app.get('/listings', (_req, res) => {
+  return res.send(listings)
+})
+
+app.post('/delete-listing', (_req, res) => {
+  const id: string = _req.body.id
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) return res.send(listings.splice(i, 1))
+  }
+
+  return res.send(`Failed to delete listing.`)
+})
+
 app.listen(port)
 
 console.log(`[app]: http://localhost:${port}`)
